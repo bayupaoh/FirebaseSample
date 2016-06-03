4940c4de24506ed75880166fca96ccb9c73f2f2a
codelabs unikom <unikom.codelabs@gmail.com>
2016-06-03 19:22:14 +0700

    first upload

---
+++ app/src/main/java/me/bayupaoh/firebasesample/config/UrlCOnfig.java
@@ -0,0 +1,10 @@
+package me.bayupaoh.firebasesample.config;
+
+/**
+ * Created by codelabsunikom on 6/3/16.
+ */
+public class UrlCOnfig {
+
+    public static final String FIREBASE_URL = "https://sampleappbayu.firebaseio.com/";
+
+}
---
+++ app/src/main/java/me/bayupaoh/firebasesample/model/ModelStudent.java
@@ -0,0 +1,26 @@
+package me.bayupaoh.firebasesample.model;
+
+/**
+ * Created by codelabsunikom on 6/3/16.
+ */
+public class ModelStudent {
+
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
